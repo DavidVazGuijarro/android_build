4c6fb3a0ad0246d10f6e3b4ed171e0f9407e0daa
David Vaz Guijarro <vazdavid@icloud.com>
2015-01-17 15:53:47 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 source build/envsetup.sh
+cd out/target/product/z3/
+rm -rf build.prop
+cd ..
+cd leo
+rm -rf build.prop
+cd ../../../..
 make installclean
 export USE_CCACHE=1
 rebuilts/misc/linux-x86/ccache/ccache -M 50G
