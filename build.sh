6e0f9f94899231097e997530ffbc445aa973e15d
David Vaz Guijarro <vazdavid@icloud.com>
2015-01-17 15:52:09 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 source build/envsetup.sh
 make installclean
+export USE_CCACHE=1
+rebuilts/misc/linux-x86/ccache/ccache -M 50G
 # leo, Xperia z3
+croot
 lunch cm_z3-userdebug && make installclean && brunch z3 -j12
